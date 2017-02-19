fdf30883cab44dde9b37cf66fb6f3e08273d2640
Omar Serrano <oser520@gmail.com>
2017-02-19 10:09:48 -0800

    Create doc for clearWarningOnInput()

--- js/util.js
+++ js/util.js
@@ -16,6 +16,11 @@ function addEvent(el, event, callback) {
   }
 }
 
+/* Removes warning message from login or register page as soon as user begins
+ * typing a new username or password.
+ * @param {string} inputId - The ID of the input element.
+ * @param {string} warningId - The ID of the warning element.
+ */
 function clearWarningOnInput(inputId, warningId) {
   var el= document.getElementById(inputId);
   addEvent(el, 'input', function() {
